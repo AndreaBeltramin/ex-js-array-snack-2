ef3bb68122aec61e352abdf4eff16ee5abab381c
AndreaBeltramin <andreabeltra99@gmail.com>
2025-03-09 14:20:30 +0100

    snack 5 bonus

---
+++ snack-5-bonus.js
@@ -0,0 +1,20 @@
+// Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id}
+// usa la combinazione di .map() e Promise.all(), per
+// creare una funzione (getBooks) che a partire da un
+// array di id (ids), ritorna una promise che risolve
+// un array di libri (books).
+// Testala con l’array [2, 13, 7, 21, 19].
+
+const ids = [2, 13, 7, 21, 19];
+
+async function getBooks(ids) {
+	const bookPromises = ids.map((id) =>
+		fetch(
+			`https://boolean-spec-frontend.vercel.app/freetestapi/books/${id}`
+		).then((res) => res.json())
+	);
+	const books = await Promise.all(bookPromises);
+	return books;
+}
+
+getBooks(ids).then((books) => console.log(books));
